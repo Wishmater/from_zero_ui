1a0c0dd4c198ae7ab34225ac5c7e991aa36ee8f5
David Alcalde <david.alcalde.cu@gmail.com>
2023-05-15 11:30:29 -0400

    Minor visual fixes.

--- lib/src/table/table_from_zero.dart
+++ lib/src/table/table_from_zero.dart
@@ -805,7 +805,7 @@ class TableFromZeroState<T> extends State<TableFromZero<T>> with TickerProviderS
         },
         child: Container(
           color: _getMaterialColor(),
-          padding: EdgeInsets.symmetric(vertical: 16),
+          padding: EdgeInsets.only(top: 4, bottom: 12),
           child: widget.emptyWidget ?? ErrorSign(
             icon: Icon(MaterialCommunityIcons.clipboard_alert_outline, size: 64, color: Theme.of(context).disabledColor,),
             title: FromZeroLocalizations.of(context).translate('no_data'),
