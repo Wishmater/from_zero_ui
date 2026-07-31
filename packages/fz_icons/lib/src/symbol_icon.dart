511f34d1e25eb125347e359bbce063d4d3e38e49
David Alcalde <david.alcalde.cu@gmail.com>
2026-07-31 14:46:40 -0500

    fz_icons: fix to not use our fork of symbols anymore

--- packages/fz_icons/lib/src/symbol_icon.dart
+++ packages/fz_icons/lib/src/symbol_icon.dart
@@ -1,11 +1,58 @@
 import "package:flutter/material.dart";
 import "package:fz_icons/src/text_icon.dart";
-import "package:material_symbols_icons/material_symbols_icons.dart";
+
+enum IconVariation {
+  outlined,
+  rounded,
+  sharp,
+}
+
+extension IconDataCopyWith on IconData {
+  IconData copyWith({
+    int? codePoint,
+    String? fontFamily,
+    String? fontPackage,
+    bool? matchTextDirection,
+    List<String>? fontFamilyFallback,
+  }) {
+    return IconData(
+      // ignore: non_const_argument_for_const_parameter
+      codePoint ?? this.codePoint,
+      // ignore: non_const_argument_for_const_parameter
+      fontFamily: fontFamily ?? this.fontFamily,
+      // ignore: non_const_argument_for_const_parameter
+      fontPackage: fontPackage ?? this.fontPackage,
+      matchTextDirection: matchTextDirection ?? this.matchTextDirection,
+      fontFamilyFallback: fontFamilyFallback ?? this.fontFamilyFallback,
+    );
+  }
+}
+
+extension SymbolIconData on IconData {
+  IconData getVariation(IconVariation variation) {
+    switch (variation) {
+      case IconVariation.outlined:
+        return copyWith(fontFamily: 'MaterialSymbolsOutlined');
+      case IconVariation.rounded:
+        return copyWith(fontFamily: 'MaterialSymbolsRounded');
+      case IconVariation.sharp:
+        return copyWith(fontFamily: 'MaterialSymbolsSharp');
+    }
+  }
+
+  Map<IconVariation, IconData> getVariations() {
+    return {
+      for (final variation in IconVariation.values) //
+        variation: getVariation(variation),
+    };
+  }
+}
 
 /// does a few things that flutter default Icon should do, but it doesn't,
 /// especially related to new material Symbols. like setting the appropriate
 /// opticalSize
 class SymbolIcon extends StatelessWidget {
+  // TODO: 2 these should be in theme, so we can react to them?
   static double defaultFlutterFill = 0;
   static double defaultFlutterWeight = 400;
   static IconVariation defaultFlutterVariation = IconVariation.outlined;
@@ -25,6 +72,7 @@ class SymbolIcon extends StatelessWidget {
     this.applyTextScaling,
     this.blendMode,
     this.fontWeight,
+    this.variation,
     this.twoTone,
   });
 
@@ -41,6 +89,8 @@ class SymbolIcon extends StatelessWidget {
   final bool? applyTextScaling;
   final BlendMode? blendMode;
   final FontWeight? fontWeight;
+  final IconVariation? variation;
+  // TODO: 1 maybe this should be a separate widget
   final bool? twoTone;
 
   @override
@@ -50,8 +100,8 @@ class SymbolIcon extends StatelessWidget {
     final fill = this.fill ?? SymbolIcon.defaultFlutterFill;
     final weight = this.weight ?? SymbolIcon.defaultFlutterWeight;
     var icon = this.icon;
-    if (icon != null && icon is VariedIconData) {
-      icon = icon.getVariation(SymbolIcon.defaultFlutterVariation);
+    if (icon != null) {
+      icon = icon.getVariation(variation ?? SymbolIcon.defaultFlutterVariation);
     }
     final mainIcon = Icon(
       icon,
@@ -68,7 +118,7 @@ class SymbolIcon extends StatelessWidget {
       blendMode: blendMode,
       fontWeight: fontWeight,
     );
-    // TODO: 1 ICONS Implement two-tone icons, requires better color declarations in theme
+    // TODO: 1 Implement two-tone icons, requires better color declarations in theme
 
     // if (!(twoTone ?? mainConfig.theme.iconFlutterTwoTone)) {
     return mainIcon;
